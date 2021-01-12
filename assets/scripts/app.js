e10b4a04d65c69669e78b72dd94e20c2628bd605
zanhorst <zanhorst@gmail.com>
2021-01-12 08:55:38 -0500

    commit
    committing for grunt deploy

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,7 +3,7 @@
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 const authEvents = require('./auth/events')
-const groceryEvents = require('../grocery/events')
+const groceryEvents = require('./grocery/events')
 // const itemEvents = require('../item/events')
 // use require without a reference to ensure a file is bundled
 // require('./example')
@@ -14,5 +14,6 @@ $(() => {
   $('#sign-out').on('submit', authEvents.onSignOut)
   $('#show-change-password').on('click', authEvents.onShowChangePassword)
   $('#change-password').on('submit', authEvents.onChangePassword)
-  $('new-list').on('click', groceryEvents.onCreateList)
+  $('#new-list').on('click', groceryEvents.onCreateList)
+  $('#index-lists').on('click', groceryEvents.onIndexGroceries)
 })
--- assets/scripts/grocery/ui.js
+++ assets/scripts/grocery/ui.js
@@ -1,41 +1,41 @@
 'use strict'
-const store = require('./../store.js')
+// const store = require('./../store.js')
 $('.authenticated').hide()
-const onIndexListsSuccess = function (response) {
+// const onIndexListsSuccess = function (response) {
 
-}
+// }
 // const onIndexListsFailure = function (error) {
 //
 // }
 
-const onShowListSuccess = function (response) {
+// const onShowListSuccess = function (response) {
 
-}
+// }
 // const onShowListFailure = function (error) {
 //
 // }
 
-const onCreateListSuccess = function (response) {
+// const onCreateListSuccess = function (response) {
 
-}
+// }
 // const onCreateListFailure = function (error) {
 //
 // }
 
-const onPatchListSuccess = function (response) {
+// const onPatchListSuccess = function (response) {
 
-}
+// }
 // const onPatchListFailure = function (error) {
 //
 // }
 
 module.exports = {
-  onIndexListsSuccess,
-  onIndexListsFailure,
-  onShowListSuccess,
-  onShowListFailure,
-  onCreateListSuccess,
-  onCreateListFailure,
-  onPatchListSuccess,
-  onPatchListFailure
+  // onIndexListsSuccess,
+  // onIndexListsFailure,
+  // onShowListSuccess,
+  // onShowListFailure,
+  // onCreateListSuccess,
+  // onCreateListFailure,
+  // onPatchListSuccess
+  // onPatchListFailure
 }
